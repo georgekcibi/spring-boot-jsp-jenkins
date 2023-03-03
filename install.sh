43bcefed70be8859d56179c350b302e506ec01e2
georgekcibi <georgycibi@gmail.com>
2023-03-03 23:36:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo kill -9 $(lsof -i:8082 -t) || true
-pwd
-cd target && sudo /usr/bin/java -jar -Dserver.port=8082 news-v1.0.4.jar &
+cd && sudo /usr/bin/java -jar -Dserver.port=8082 target/*.jar &
