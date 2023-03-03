ff5a23ed7bcfa426a27309c5eef7e8a9f365d55b
georgekcibi <georgycibi@gmail.com>
2023-03-03 23:45:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo kill -9 $(lsof -i:8082 -t) || true
-sudo cd /home/ubuntu && sudo /usr/bin/java -jar -Dserver.port=8082 target/*.jar &
+cd /home/ubuntu && sudo /usr/bin/java -jar -Dserver.port=8082 target/*.jar &
